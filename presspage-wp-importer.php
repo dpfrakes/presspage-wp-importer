8f195b90475ed264c4cbf46105f00fd0e3bdce79
dpfrakes <dpfrakes@gmail.com>
2018-09-25 13:43:15 -0400

    Use wp_schedule_single_event instead of async libs

--- presspage-wp-importer.php
+++ presspage-wp-importer.php
@@ -47,122 +47,119 @@ if (!function_exists('featured_image_trick')) {
 	}
 }
 
-if (!class_exists('PresspageImport')) {
-	// Class to run asynchronously
-	class PresspageImport extends WP_Async_Request {
+if (!function_exists('run_presspage_import')) {
+	// Run import asynchronously
+	function run_presspage_import() {
 
-		public $action = 'presspage_wp_importer_async_process';
+		// Disable a time limit
+		set_time_limit(0);
 
-		protected function handle() {
+		// Data
+		try {
+			$file_contents = file_get_contents(plugin_dir_path(__FILE__) . 'export-account/feeds/releases-en-us.json');
+			$json_content = json_decode('' . $file_contents, true);
+		} catch (Exception $e) {
+			echo "Error retrieving and/or decoding JSON export file.";
+			echo "Exception message: ", $e->getMessage(), "\n";
+			exit(1);
+		}
 
-			// Disable a time limit
-			set_time_limit(0);
+		// Require some Wordpress core files for processing images
+		require_once(ABSPATH . 'wp-admin/includes/media.php');
+		require_once(ABSPATH . 'wp-admin/includes/file.php');
+		require_once(ABSPATH . 'wp-admin/includes/image.php');
 
-			// Data
-			try {
-				$file_contents = file_get_contents(plugin_dir_path(__FILE__) . 'export-account/feeds/releases-en-us.json');
-				$json_content = json_decode('' . $file_contents, true);
-			} catch (Exception $e) {
-				echo "Error retrieving and/or decoding JSON export file.";
-				echo "Exception message: ", $e->getMessage(), "\n";
-				exit(1);
-			}
+		// Succesfully loaded?
+		if($json_content !== FALSE) {
 
-			// Require some Wordpress core files for processing images
-			require_once(ABSPATH . 'wp-admin/includes/media.php');
-			require_once(ABSPATH . 'wp-admin/includes/file.php');
-			require_once(ABSPATH . 'wp-admin/includes/image.php');
+			// Loop through some items in the json
+			foreach($json_content as $k => $v) {
 
-			// Succesfully loaded?
-			if($json_content !== FALSE) {
+				try {
+					// Get object
+					$presspage_post = $json_content[$k];
 
-				// Loop through some items in the json
-				foreach($json_content as $k => $v) {
-
-					try {
-						// Get object
-						$presspage_post = $json_content[$k];
-
-						// DEBUGGING
-						if ( !array_key_exists('title', $presspage_post) ) {
-							throw new Exception('Entry ' . $k + 1 . ' does not contain title');
-						}
-
-						if ( !array_key_exists('message', $presspage_post) ) {
-							throw new Exception('Entry ' . $k + 1 . ' does not contain message');
-						}
-
-						$excerpt = wp_trim_words(strip_tags($presspage_post['message'], '<a>'), 60);
-						if ( array_key_exists('summary', $presspage_post) ) {
-							$excerpt = wp_trim_words(strip_tags($presspage_post['summary'], '<a>'), 60);
-						} else if ( strpos($excerpt, '<!--more') != false ) {
-							$excerpt = substr(strip_tags($presspage_post['summary'], '<a>'), 0, strpos($excerpt, '<!--more'));
-						}
-
-						// Let's start with creating the post itself
-						$postCreated = array(
-							'post_title'    => $presspage_post['title'],
-							'post_content'  => $presspage_post['message'],
-							'post_excerpt'  => $excerpt,
-							'post_date'     => $presspage_post['date'],
-							'post_status'   => 'publish',
-							'post_type'     => 'post', // Or "page" or some custom post type
-						);
-
-						// Get the increment id from the inserted post
-						$postInsertId = wp_insert_post( $postCreated );
-
-						// Our custom post options, for now only some meta's for the
-						// Yoast SEO plugin and a "flag" to determined if a
-						// post was imported or not
-						$postOptions = array(
-							'_yoast_wpseo_title'    => $presspage_post['title'],
-							'imported'              => true
-						);
-
-						// Loop through the post options
-						foreach($postOptions as $key => $value){
-
-							// Add the post options
-							update_post_meta($postInsertId, $key, $value);
-						}
-
-						// This is a little trick to "catch" the image id
-						// Attach/upload the "sideloaded" image
-						// And remove the little trick
-						if ( array_key_exists('images', $presspage_post) && count(array_keys($presspage_post['images'])) > 0 ) {
-
-							// Point to last image (assumed to be local copy/highest res/latest version)
-							$pp_post_images = $presspage_post['images'];
-							end($pp_post_images);
-							$post_image_url = $pp_post_images[key($pp_post_images)];
-
-							// Check if image is URL or local file reference
-							if ( substr($post_image_url, 0, 2) == '//' ) {
-								// Media server
-								media_sideload_image('http:' . $post_image_url, $postInsertId);
-							} else {
-								// Local
-								media_sideload_image(plugin_dir_path(__FILE__) . 'export-account/uploads/' . $post_image_url, $postInsertId);
-							}
-
-							add_action('add_attachment', 'featured_image_trick');
-							media_sideload_image($pp_post_images[key($pp_post_images)], $postInsertId);
-							remove_action('add_attachment', 'featured_image_trick');
-
-						}
-
-					} catch (Exception $exc) {
-						echo $exc->getMessage() . '<br/>';
+					// DEBUGGING
+					if ( !array_key_exists('title', $presspage_post) ) {
+						throw new Exception('Entry ' . $k + 1 . ' does not contain title');
 					}
+
+					if ( !array_key_exists('message', $presspage_post) ) {
+						throw new Exception('Entry ' . $k + 1 . ' does not contain message');
+					}
+
+					$excerpt = wp_trim_words(strip_tags($presspage_post['message'], '<a>'), 60);
+					if ( array_key_exists('summary', $presspage_post) ) {
+						$excerpt = wp_trim_words(strip_tags($presspage_post['summary'], '<a>'), 60);
+					} else if ( strpos($excerpt, '<!--more') != false ) {
+						$excerpt = substr(strip_tags($presspage_post['summary'], '<a>'), 0, strpos($excerpt, '<!--more'));
+					}
+
+					// Let's start with creating the post itself
+					$postCreated = array(
+						'post_title'    => $presspage_post['title'],
+						'post_content'  => $presspage_post['message'],
+						'post_excerpt'  => $excerpt,
+						'post_date'     => $presspage_post['date'],
+						'post_status'   => 'publish',
+						'post_type'     => 'post', // Or "page" or some custom post type
+					);
+
+					// Get the increment id from the inserted post
+					$postInsertId = wp_insert_post( $postCreated );
+
+					// Our custom post options, for now only some meta's for the
+					// Yoast SEO plugin and a "flag" to determined if a
+					// post was imported or not
+					$postOptions = array(
+						'_yoast_wpseo_title'    => $presspage_post['title'],
+						'imported'              => true
+					);
+
+					// Loop through the post options
+					foreach($postOptions as $key => $value){
+
+						// Add the post options
+						update_post_meta($postInsertId, $key, $value);
+					}
+
+					// This is a little trick to "catch" the image id
+					// Attach/upload the "sideloaded" image
+					// And remove the little trick
+					if ( array_key_exists('images', $presspage_post) && count(array_keys($presspage_post['images'])) > 0 ) {
+
+						// Point to last image (assumed to be local copy/highest res/latest version)
+						$pp_post_images = $presspage_post['images'];
+						end($pp_post_images);
+						$post_image_url = $pp_post_images[key($pp_post_images)];
+
+						// Check if image is URL or local file reference
+						if ( substr($post_image_url, 0, 2) == '//' ) {
+							// Media server
+							media_sideload_image('http:' . $post_image_url, $postInsertId);
+						} else {
+							// Local
+							media_sideload_image(plugin_dir_path(__FILE__) . 'export-account/uploads/' . $post_image_url, $postInsertId);
+						}
+
+						add_action('add_attachment', 'featured_image_trick');
+						media_sideload_image($pp_post_images[key($pp_post_images)], $postInsertId);
+						remove_action('add_attachment', 'featured_image_trick');
+
+					}
+
+				} catch (Exception $exc) {
+					echo $exc->getMessage() . '<br/>';
 				}
 			}
-
-			// Deactivate plugin when import finishes
-			deactivate_plugins(plugin_basename(__FILE__));
 		}
+
+		// Deactivate plugin when import finishes
+		deactivate_plugins(plugin_basename(__FILE__));
 	}
 }
+
+add_action( 'trigger_import', 'run_presspage_import' );
 
 if (!function_exists('presspage_wp_importer_run_import')) {
 	function presspage_wp_importer_run_import() {
@@ -171,8 +168,7 @@ if (!function_exists('presspage_wp_importer_run_import')) {
 		set_transient('presspage-import-admin-notice', true, 5);
 
 		// Begin import as async process
-		$import = new PresspageImport();
-		$import->dispatch();
+		wp_schedule_single_event( time(), 'trigger_import' );
 	}
 }
 
@@ -181,6 +177,7 @@ if (!function_exists('presspage_wp_importer_import_complete')) {
 		add_action( 'admin_notices', 'presspage_wp_importer_deactivation_message' );
 	}
 }
+
 register_activation_hook( __FILE__, 'presspage_wp_importer_run_import' );
 register_deactivation_hook( __FILE__, 'presspage_wp_importer_import_complete' );
 
@@ -206,6 +203,9 @@ if (!function_exists('presspage_wp_importer_deactivation_message')) {
 		echo '<div class="notice notice-success is-dismissible"><p>Import finished. Presspage import plugin has been deactivated.</p></div>';
 	}
 }
+
+
+
 
 /* debug wordpress unhelpful error messages */
 // https://www.toddlahman.com/the-plugin-generated-x-characters-of-unexpected-output-during-activation/
