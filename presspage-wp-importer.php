b05f7b6e0237cf32cba88cb5afa2540ce0e072c4
dpfrakes <dpfrakes@gmail.com>
2018-09-25 14:36:00 -0400

    Cancel import run on plugin deactivation

--- presspage-wp-importer.php
+++ presspage-wp-importer.php
@@ -174,6 +174,8 @@ if (!function_exists('presspage_wp_importer_run_import')) {
 
 if (!function_exists('presspage_wp_importer_import_complete')) {
 	function presspage_wp_importer_import_complete() {
+		// Cancel import
+		wp_clear_scheduled_hook( 'trigger_import' );
 		add_action( 'admin_notices', 'presspage_wp_importer_deactivation_message' );
 	}
 }
@@ -188,11 +190,7 @@ if (!function_exists('presspage_wp_importer_activation_message')) {
 
 		/* Check transient, if available display notice */
 		if( get_transient( 'presspage-import-admin-notice' ) ){
-			echo '<div class="notice notice-success is-dismissible"><p>Presspage import has started. Plugin will automatically deactivate when import is complete.</p>
-				<button type="button" class="notice-dismiss">
-					<span class="screen-reader-text">Dismiss this notice.</span>
-				</button>
-			</div>';
+			echo '<div class="notice notice-success is-dismissible"><p>Presspage import has started. Plugin will automatically deactivate when import is complete.</p></div>';
 			delete_transient( 'presspage-import-admin-notice' );
 		}
 	}
@@ -203,17 +201,3 @@ if (!function_exists('presspage_wp_importer_deactivation_message')) {
 		echo '<div class="notice notice-success is-dismissible"><p>Import finished. Presspage import plugin has been deactivated.</p></div>';
 	}
 }
-
-
-
-
-/* debug wordpress unhelpful error messages */
-// https://www.toddlahman.com/the-plugin-generated-x-characters-of-unexpected-output-during-activation/
-if (!function_exists('tl_save_error')) {
-	function tl_save_error() {
-		update_option( 'plugin_error',  ob_get_clean() );
-	}
-}
-
-add_action( 'activated_plugin', 'tl_save_error' );
-echo get_option( 'plugin_error' );
